e44bf9e67f233cd45b0736175b9741f6516ff39d
PiwoZz <ocuspocuslotus@hotmail.com>
2026-02-17 23:59:07 +0100

    TwitchChat working now

--- Assets/Scripts/StreamingChatManager.cs
+++ Assets/Scripts/StreamingChatManager.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class StreamingChatManager : MonoBehaviour
 {
+    [SerializeField] private StringList usernameList;
+    [SerializeField] private StringList messageList;
+    
     [SerializeField] private GameObject layout;
     [SerializeField] private GameObject chatMessagePrefab;
     [SerializeField] private float maximumBeforeMessage;
     [SerializeField] private float minimumBeforeMessage;
+    [SerializeField] private int maximumMessages = 12;
 
     private float currentDelay;
     private float timeInSeconds;
     private float timer;
+    private float messageSize;
 
     private void Start()
     {
         ChooseNewDelay();
+
+        messageSize = layout.GetComponent<RectTransform>().rect.height / maximumMessages;
     }
 
     private void Update()
@@ -39,17 +46,24 @@ public class StreamingChatManager : MonoBehaviour
 
     private void CraftMessage()
     {
-        GameObject go =  Instantiate(chatMessagePrefab, layout.transform);
-        var tmp = go.GetComponentInChildren<TextMeshProUGUI>();
         string templateText = "streamtime username: text";
-        templateText = templateText.Replace("streamtime", "<color=#595959>" + timeInSeconds.ToString("0:00") + "</color>");
+
+        float minutes = timeInSeconds / 60;
+        templateText = templateText.Replace("streamtime", $"<color=#595959>{Mathf.Floor(timeInSeconds / 60f):00}:{(timeInSeconds % 60):00}</color>");
         
         Color randomColor = Random.ColorHSV();
         string hex = randomColor.ToHexString();
-        templateText = templateText.Replace("username", $"<color=#{hex}>" + "ItsAMe" + "</color>");
+        templateText = templateText.Replace("username", $"<color=#{hex}>" + usernameList.stringList[Random.Range(0, usernameList.stringList.Count)] + "</color>");
         
-        templateText = templateText.Replace("text",  "Just wanna say I love you");
-        
+        templateText = templateText.Replace("text",  messageList.stringList[Random.Range(0, messageList.stringList.Count)]);
+
+        if (layout.transform.childCount > maximumMessages)
+            Destroy(layout.transform.GetChild(0).gameObject);
+
+        GameObject go =  Instantiate(chatMessagePrefab, layout.transform);
+        var rectTransform = go.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, messageSize);
+        var tmp = go.GetComponentInChildren<TextMeshProUGUI>();
         tmp.text = templateText;
     }
 }
---
+++ Assets/Scripts/StringList.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StringList", menuName = "StringList")]
+    public class StringList : ScriptableObject
+    {
+        public List<string> stringList;
+    }
